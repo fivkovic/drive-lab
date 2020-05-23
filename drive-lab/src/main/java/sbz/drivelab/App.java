1379d2d39fdf39a138137f94e5706dd0ab469655
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-24 00:39:01 +0200

    Added Spring Boot support

--- drive-lab/src/main/java/sbz/drivelab/App.java
+++ drive-lab/src/main/java/sbz/drivelab/App.java
@@ -1,11 +1,13 @@
 package sbz.drivelab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
     }
 }
