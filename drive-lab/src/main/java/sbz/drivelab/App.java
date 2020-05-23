fca4c39fd60b7829fd05e2e4d3b0f289b22a7d61
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-23 19:09:30 +0200

    Created initial project structure

---
+++ drive-lab/src/main/java/sbz/drivelab/App.java
@@ -0,0 +1,11 @@
+package sbz.drivelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
