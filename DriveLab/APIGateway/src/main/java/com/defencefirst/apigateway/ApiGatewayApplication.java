309b9cdbce15b091fbea75873e4d1d3b099e8f19
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-25 00:01:12 +0200

    Added initial version of API Gateway reverse-proxy service

---
+++ DriveLab/APIGateway/src/main/java/com/defencefirst/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,35 @@
+package com.defencefirst.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
