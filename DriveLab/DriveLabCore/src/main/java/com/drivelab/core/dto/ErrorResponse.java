5258d1372a9721822515719e6e1561fddad7fe6f
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-29 01:27:19 +0200

    Implemented global exception handling

---
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.drivelab.core.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String message;
+
+}
---
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package com.drivelab.core.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
