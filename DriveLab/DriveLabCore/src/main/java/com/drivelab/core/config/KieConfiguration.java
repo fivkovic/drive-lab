9edeb622bc270ecfa5382592eca37289f7f6adcc
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-29 01:34:19 +0200

    Added initial security and kie configurations

---
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/config/KieConfiguration.java
@@ -0,0 +1,37 @@
+package com.drivelab.core.config;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieBaseConfiguration;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KieConfiguration {
+
+    @Value("${rules.groupId}")
+    private String groupId;
+
+    @Value("${rules.artifactId}")
+    private String artifactId;
+
+    @Value("${rules.version}")
+    private String version;
+
+    @Bean
+    public KieBase kieBase() {
+        final KieServices kieServices = KieServices.Factory.get();
+        final KieBaseConfiguration kieBaseConfig = kieServices.newKieBaseConfiguration();
+        kieBaseConfig.setOption(EventProcessingOption.STREAM);
+
+        final KieContainer kieContainer = kieServices.newKieContainer(kieServices.newReleaseId(groupId, artifactId, version));
+        final KieScanner kieScanner = kieServices.newKieScanner(kieContainer);
+        kieScanner.start(5000);
+
+        return kieContainer.newKieBase(kieBaseConfig);
+    }
+}
---
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.drivelab.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin().disable();
+        http.httpBasic().disable();
+        http.csrf().disable();
+    }
+}
