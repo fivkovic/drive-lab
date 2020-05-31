82f8e2eeca9fe49a65d9f114df197102802621c3
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-05-31 18:07:11 +0200

    Added Bootstrap and SweetAlert dependencies on client

--- DriveLab/drive-lab-client/src/main.js
+++ DriveLab/drive-lab-client/src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import Multiselect from 'vue-multiselect';
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueSweetalert2)
+Vue.component('multiselect', Multiselect)
 
 new Vue({
   router,
