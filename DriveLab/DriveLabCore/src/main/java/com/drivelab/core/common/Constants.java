8ded21d6922b9992a52879842d636ef5ec5bc2e0
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-06-01 00:27:55 +0200

    Implemented BaseController and BaseService

--- DriveLab/DriveLabCore/src/main/java/com/drivelab/core/common/Constants.java
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/common/Constants.java
@@ -7,5 +7,8 @@ public class Constants {
     public static class API {
 
         public static final String AUTHENTICATION_ENDPOINT = "/api/auth";
+        public static final String VEHICLES_ENDPOINT = "/api/vehicles";
+        public static final String VEHICLE_MANUFACTURERS_ENDPOINT = "/api/vehicle-manufacturers";
+        public static final String CUSTOMERS_ENDPOINT = "/api/customers";
     }
 }
---
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/controller/BaseController.java
@@ -0,0 +1,42 @@
+package com.drivelab.core.controller;
+
+import com.drivelab.core.service.BaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+public abstract class BaseController<T, ID> {
+
+    protected final BaseService<T, ID> service;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody @Valid T payload) {
+        return new ResponseEntity<>(this.service.create(payload), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(this.service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable ID id) {
+        return ResponseEntity.ok(this.service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody @Valid T payload, @PathVariable ID id) {
+        return ResponseEntity.ok(this.service.update(id, payload));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable ID id) {
+        this.service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
