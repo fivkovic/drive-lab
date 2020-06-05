a36c4f11cb758a2926389ba3b1645e1999293c50
Filip Ivkovic <fivkovic@uns.ac.rs>
2020-06-05 17:24:30 +0200

    Updated endpoints

--- DriveLab/DriveLabCore/src/main/java/com/drivelab/core/common/Constants.java
+++ DriveLab/DriveLabCore/src/main/java/com/drivelab/core/common/Constants.java
@@ -6,9 +6,13 @@ public class Constants {
 
     public static class API {
 
-        public static final String AUTHENTICATION_ENDPOINT = "/api/auth";
+        public static final String AUTHENTICATION_ENDPOINT = "/api/authentication";
+        public static final String DIAGNOSTICS_ENDPOINT = "/api/diagnostics";
+
         public static final String VEHICLES_ENDPOINT = "/api/vehicles";
         public static final String VEHICLE_MANUFACTURERS_ENDPOINT = "/api/vehicle-manufacturers";
         public static final String CUSTOMERS_ENDPOINT = "/api/customers";
+        public static final String SYMPTOMS_ENDPOINT = "/api/symptoms";
+        public static final String CAR_PARTS_ENDPOINT = "/api/car-parts";
     }
 }
